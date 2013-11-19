b77480a3c06dc5380ab07cd2df21ca12cd1f7c8b
Greg Allen <me@jga.me>
2013-11-19 15:08:31 -0800

    updated module name to ftPopbox.  Removed isolate scope, use popbox attribute instead of popbox-text.  Fixes #1. Fixes #2

--- example/app.js
+++ example/app.js
@@ -1,6 +1,12 @@
-angular.module('example-app', ['popbox']);
+angular.module('example-app', ['ftPopbox']);
 
 var MainController = function($scope) {
   $scope.text1 = 'This is example 1';
   $scope.text2 = 'This is example 2';
+
+
+  $scope.update = function() {
+    $scope.text1 = 'this is an updated example 1';
+    $scope.text2 = 'this is an updated example 2';
+  };
 };
