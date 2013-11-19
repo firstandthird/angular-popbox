e89c0613010a3b8aa71c3872251cab5f796a4732
Greg Allen <me@jga.me>
2013-11-19 15:11:16 -0800

    updated build scripts.  fixed #3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,147 +1,15 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    info: grunt.file.readJSON('bower.json'),
-    meta: {
-      banner: '/*!\n'+
-              ' * <%= info.name %> - <%= info.description %>\n'+
-              ' * v<%= info.version %>\n'+
-              ' * <%= info.homepage %>\n'+
-              ' * copyright <%= info.copyright %> <%= grunt.template.today("yyyy") %>\n'+
-              ' * <%= info.license %> License\n'+
-              '*/\n'
-    },
-    jshint: {
-      main: [
-        'Gruntfile.js',
-        'bower.json',
-        'lib/**/*.js',
-        'test/*.js'
-      ]
-    },
-    bower: {
-      main: {
-        dest: 'dist/_bower.js',
-        exclude: [
-          'assert',
-          'jquery',
-          'angular'
-        ]
-      }
-    },
-    concat: {
-      options: {
-        banner: '<%= meta.banner %>'
-      },
-      dist: {
-        src: [
-          'lib/angular-popbox.js'
-        ],
-        dest: 'dist/angular-popbox.js'
-      },
-      full: {
-        src: [
-          'dist/_bower.js',
-          'lib/angular-popbox.js'
-        ],
-        dest: 'dist/angular-popbox.full.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= meta.banner %>'
-      },
-      dist: {
-        src: 'dist/angular-popbox.js',
-        dest: 'dist/angular-popbox.min.js'
-      },
-      full: {
-        src: 'dist/angular-popbox.full.js',
-        dest: 'dist/angular-popbox.full.min.js'
-      }
-    },
-    clean: {
-      bower: [
-        'dist/_bower.js'
-      ],
-      dist: [
-        'dist'
-      ]
-    },
-    watch: {
-      scripts: {
-        files: '<%= jshint.main %>',
-        tasks: 'scripts',
-        options: {
-          livereload: true
-        }
-      },
-      example: {
-        files: [
-          'example/*'
-        ],
-        options: {
-          livereload: true
-        }
-      },
-      grunt: {
-        files: [
-          'Gruntfile.js',
-          'test/index.html'
-        ],
-        tasks: 'default'
-      }
-    },
-    mocha: {
-      all: {
-        src: 'test/index.html',
-        options: {
-          run: true,
-          growl: true
-        }
-      }
-    },
-    plato: {
-      main: {
-        files: {
-          'reports': ['lib/*.js']
-        }
-      }
-    },
-    connect: {
-      server:{
-        options: {
-          hostname: '*'
-        }
-      },
-      plato: {
-        port: 8000,
-        base: 'reports',
-        options: {
-          keepalive: true
-        }
-      }
-    },
-    bytesize: {
-      scripts: {
-        src: [
-          'dist/*'
-        ]
-      }
+
+  require('load-grunt-config')(grunt, {
+    config: {
+      info: grunt.file.readJSON('bower.json'),
+      name: 'angular-popbox'
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-concat-bower');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-bytesize');
-  grunt.loadNpmTasks('grunt-mocha');
-  grunt.loadNpmTasks('grunt-plato');
-  grunt.registerTask('scripts', ['jshint', 'bower', 'concat', 'uglify', 'clean:bower', 'mocha', 'bytesize']);
+
+  grunt.registerTask('script-dist', ['concat:dist', 'uglify:dist']);
+  grunt.registerTask('script-full', ['concat:full', 'uglify:full']);
+  grunt.registerTask('scripts', ['jshint', 'bower', 'script-dist', 'script-full', 'clean:bower', 'mocha', 'bytesize', 'notify:generic']);
   grunt.registerTask('default', ['scripts']);
-  grunt.registerTask('dev', ['connect:server', 'scripts', 'watch']);
-  grunt.registerTask('reports', ['plato', 'connect:plato']);
+  grunt.registerTask('dev', ['default', 'connect:server', 'notify:watch', 'watch']);
 };
