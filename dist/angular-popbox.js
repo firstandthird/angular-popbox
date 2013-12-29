f8f715ef3f51b598b74d205981c77a9047b27fd0
Greg Allen <me@jga.me>
2013-12-29 12:32:21 -0800

    Release 0.3.1

--- dist/angular-popbox.js
+++ dist/angular-popbox.js
@@ -1,13 +1,13 @@
 
 /*!
  * angular-popbox - Angular directive for popbox plugin
- * v0.3.0
+ * v0.3.1
  * https://github.com/firstandthird/angular-popbox
  * copyright First + Third 2013
  * MIT License
 */
 angular.module('ftPopbox', [])
-  .directive('popbox', function($parse) {
+  .directive('popbox', function() {
     return {
       restrict: 'A',
       link : function(scope, el, attrs){
@@ -29,10 +29,16 @@ angular.module('ftPopbox', [])
           updateElement(newValue);
         });
 
-        scope.$on('$destroy', function(){
-          stopObserving();
-          cls.destroy();
-        });
+        var destroyDirective = function(){
+          if (cls){
+            stopObserving();
+            cls.destroy();
+            cls = null;
+          }
+        };
+
+        scope.$on('$destroy', destroyDirective);
+        el.one('$destroy', destroyDirective);
       }
     };
   });
